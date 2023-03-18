bd61c0e219856211f995f6eb386923445bc76447
kazuma_kikuya <61053639+kazumax75@users.noreply.github.com>
2023-03-18 11:29:19 +0900

    型定義ファイル追加

---
+++ types/User.ts
@@ -0,0 +1,27 @@
+/**
+ * Userオブジェクトの型定義
+ * https://jsonplaceholder.typicode.com/users/1のレスポンスをもとに型定義
+ */
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
---
+++ types/index.ts
@@ -0,0 +1,6 @@
+export interface TestType {
+  id: string;
+  url: string;
+  width: number;
+  height: number;
+}
